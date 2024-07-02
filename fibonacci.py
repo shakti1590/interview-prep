d8e6bdaa05ae316019bb8c0317dd03fdfe7b71cd
shakti singh <60788912+shakti1590@users.noreply.github.com>
2024-07-02 23:20:39 +0530

    Fibonacci sequence in Python

---
+++ fibonacci.py
@@ -0,0 +1,8 @@
+def fibonacci_dynamic(n):
+    fib_sequence = [0, 1]
+    for i in range(2, n):
+        fib_sequence.append(fib_sequence[i-1] + fib_sequence[i-2])
+    return fib_sequence[:n]
+
+n = 10
+print(fibonacci_dynamic(n))
